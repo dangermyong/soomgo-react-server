c02bae8d140a761829b9fde647120dbbc304f804
dangermyong <66460964+dangermyong@users.noreply.github.com>
2020-09-29 11:58:06 +0900

    cors

--- app.js
+++ app.js
@@ -19,13 +19,6 @@ app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(cookieParser())
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
-
 app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true,
--- controllers/auth.js
+++ controllers/auth.js
@@ -77,6 +77,7 @@ exports.checkLogin = (req, res) => {
         res.status(200).json({ user: decodedToken, message: 'sign in successfully'})
       }
     })
-  }
+  } else {
   console.log(`Token not Found: ${token}`)
+  }
 }
