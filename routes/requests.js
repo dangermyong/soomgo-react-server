3b817c764768fc4044351a83e68b4436761b28db
dangermyong <66460964+dangermyong@users.noreply.github.com>
2020-09-29 12:26:09 +0900

    asdf

--- routes/requests.js
+++ routes/requests.js
@@ -8,8 +8,8 @@ router.post('/sent', async (req, res, next) => {
   try{
     const id = req.body.id
     const connection = await pool.getConnection();
-    const [user] = await connection.query('SELECT * FROM USER_TB WHERE id = ?', [id]);
-    const [results] = await connection.query('SELECT * FROM REQUEST_TB WHERE userId = ?', [id])
+    const [user] = await connection.query('SELECT * FROM USER_TB WHERE id = ?', id);
+    const [results] = await connection.query('SELECT * FROM REQUEST_TB WHERE userId = ?', id)
     const promises = results.map(async result => {
       const [estimates] = await connection.query('SELECT * FROM ESTIMATES_TB JOIN GOSU_TB ON ESTIMATES_TB.gosuId = GOSU_TB.id WHERE ESTIMATES_TB.requestId = ?', result.id)
       return ({
@@ -20,10 +20,10 @@ router.post('/sent', async (req, res, next) => {
     const newResults = await Promise.all(promises)
     console.log(newResults)
     connection.release()
-    res.status(200).json({ userName: user.name , results: newResults});
+    res.status(200).json({ results: newResults});
   } catch (err) {
     console.log(err);
-    res.status(500).json({ msg : '리퀘스트 에러가 났어요!'});
+    res.status(500).json({ msg : '리퀘스트 에러가 났어요!'})
   }
 });
 
