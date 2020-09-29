85b9255984540eb0f5811a85a6d4bcd75101a1f2
dangermyong <66460964+dangermyong@users.noreply.github.com>
2020-09-29 09:57:54 +0900

    a

--- app.js
+++ app.js
@@ -17,16 +17,17 @@ const app = express()
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(cookieParser())
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-}))
 
 app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next()
 })
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}))
 
   //routes middleware
 app.use('/api', authRoutes)
