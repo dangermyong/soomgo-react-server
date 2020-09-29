6a73f92de2fba12d71d915ec5d10bd890d750a7a
dangermyong <66460964+dangermyong@users.noreply.github.com>
2020-09-29 09:56:29 +0900

    a

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ app.use(cors({
   credentials: true,
 }))
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+})
+
   //routes middleware
 app.use('/api', authRoutes)
 app.use('/api', searchRoutes)
