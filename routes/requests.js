45c1bf48c2d2ccbfce138fa3ca07f75fb634af71
dangermyong <66460964+dangermyong@users.noreply.github.com>
2020-09-29 12:37:41 +0900

    quotes

--- routes/requests.js
+++ routes/requests.js
@@ -28,4 +28,29 @@ router.post('/sent', async (req, res, next) => {
   }
 });
 
+
+router.get('/quotes/:requestId', async (req, res, next) => {
+  try{
+    const id = req.body.id
+    console.log(id)
+    const connection = await pool.getConnection();
+    const [user] = await connection.query('SELECT * FROM USER_TB WHERE id = ?', [id]);
+    const [results] = await connection.query('SELECT * FROM REQUEST_TB WHERE id = ?', req.params.requestId)
+    const promises = results.map(async result => {
+      const [estimates] = await connection.query('SELECT * FROM ESTIMATES_TB JOIN GOSU_TB ON ESTIMATES_TB.gosuId = GOSU_TB.id WHERE ESTIMATES_TB.requestId = ?', req.params.requestId)
+      return ({
+        ...result,
+        estimates: estimates
+      })
+    })
+    const newResults = await Promise.all(promises)
+    console.log(newResults)
+    connection.release()
+    res.status(200).json({ userName: req.token.name , results: newResults});
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg : '쿼트 에러가 났어요!'});
+  }
+});
+
 module.exports = router;
